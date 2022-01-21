c50210582934119b28f19a65104249e553b68321
unknown <carl.nettelblad@gmail.com>
2022-01-22 00:13:10 +0100

    Init

---
+++ Database/Default.lua
@@ -0,0 +1,24 @@
+--[[
+    Default Settings
+]]
+PHTDB_DEFAULTS = {
+    Font = {
+        Handle = "Friz Quadrata TT",
+        Path = nil,
+        Size = 13,
+        Outline = "OUTLINE",
+        Color = {
+            r = 0.3,
+            g = 0.5,
+            b = 0.9,
+            a = 1,
+        }
+    }
+}
+
+PHTCDB_DEFAULTS = {
+    Pet = {
+        Happiness = "Sad",
+        HappinessPoints = 1050,
+    }
+}
---
+++ PetHappinessTracker.lua
@@ -0,0 +1,22 @@
+--[[
+    Local vars
+--]]
+local PHT = LibStub("AceAddon-3.0"):NewAddon("PetHappinessTracker")
+local SharedMedia = LibStub("LibSharedMedia-3.0")
+local frames = {}
+local localizedClass, englishClass, classIndex = UnitClass("player")
+
+--[[
+    Addon init
+--]]
+function PHT:OnInitialize()
+    if classIndex ~= 3 then
+       return -- No need to init further for non-hunters.
+    end
+    PHTDB = PHTDB_DEFAULTS
+    PHTCDB = PHTCDB_DEFAULTS
+end
+
+function PHT:OnEnable()
+
+end
