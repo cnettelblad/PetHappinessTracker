bdcc170757cc890e17d2059b96ae30b48a865bf1
unknown <carl.nettelblad@gmail.com>
2022-01-22 00:16:25 +0100

    Added PetHappiness service

---
+++ Services/PetHappiness.lua
@@ -0,0 +1,89 @@
+local PHT = LibStub("AceAddon-3.0"):GetAddon("PetHappinessTracker")
+local IdleTimer
+
+local function AddPoints(amount)
+    PHTCDB.Pet.HappinessPoints = math.min(
+        1050,
+        PHTCDB.Pet.HappinessPoints + amount
+    )
+    PHT:UpdateStatusFrame()
+end
+
+local function DeductPoints(amount)
+    PHTCDB.Pet.HappinessPoints = math.max(
+        0,
+        PHTCDB.Pet.HappinessPoints - amount
+    )
+    PHT:UpdateStatusFrame()
+end
+
+local function SetMaxPoints()
+    PHTCDB.Pet.HappinessPoints = 1050
+    PHT:UpdateStatusFrame()
+end
+
+local function EnergizeEvent(amount)
+    local validAmounts = {
+        8,  -- Lowest possible
+        10, -- Kibbler's bits
+        17, -- Medium (low-level) food
+        35  -- Maximum level food
+    }
+    for k, v in pairs(validAmounts) do
+        if v == amount then 
+            AddPoints(amount)
+            return
+        end
+    end
+    --[[
+        If the above numbers aren't matched
+        it means that the pet reached maximum
+        happiness.
+    --]]
+    SetMaxPoints()
+end
+
+function PHT:CLEUEvent(
+    timestamp,
+    subevent,
+    _,
+    sourceGUID,
+    sourceName,
+    sourceFlags,
+    sourceRaidFlags,
+    destGUID,
+    destName,
+    destFlags,
+    destRaidFlags,
+    ...
+)
+    -- No need to run when pet isn't affected
+    if not UnitExists("pet") or destGUID ~= UnitGUID("pet") then 
+        return 
+    end
+
+    -- Feeding events
+    if (subevent == "SPELL_PERIODIC_ENERGIZE" or subevent == "SPELL_ENERGIZE") then
+        local amount = select(4, ...)
+        return EnergizeEvent(amount)
+    end
+
+    -- Pet death (Ignores Steam Tonk)
+    if subEvent == "UNIT_DIED" and destName ~= "Steam Tonk" then
+        return DeductPoints(350)
+    end
+
+    -- Pet Dismiss
+    if subEvent == "SPELL_DRAIN" and sourceName == UnitName("player") then 
+        return DeductPoints(50)
+    end
+end
+
+function PHT:StartIdleTimer()
+    IdleTimer = C_Timer.NewTicker(
+        7.2,
+        function()
+            DeductPoints(1)
+        end
+    )
+end
