fa5db28004e719ff1e9700d1b1923bd623ae12fe
unknown <carl.nettelblad@gmail.com>
2022-01-22 14:47:35 +0100

    Added some basic benchmark functionality

--- Services/PetHappiness.lua
+++ Services/PetHappiness.lua
@@ -80,11 +80,57 @@ function PHT:CLEUEvent(
     end
 end
 
+function PHT:HappinessEvent()
+    print("HappinessEvent fired")
+    -- Happiness value 1-3 (1 = Unhappy, 2 = Content, 3 = Happy)
+    local happiness = GetPetHappiness()
+    local happinessPoints = ({0, 350, 750})[happiness]
+
+    -- Logic for when HappinessLevel is increased
+    print(happiness, PHTCDB.Pet.HappinessLevel)
+    if happiness > PHTCDB.Pet.HappinessLevel then
+        PHTCDB.Pet.HappinessLevel = happiness
+        if PHTCDB.Benchmark ~= nil then
+            PHTCDB.Benchmark.LatestMin = time()
+            print(PHTCDB.Benchmark.LatestMax - PHTCDB.Benchmark.LatestMin)
+        end
+
+        -- Assuming the UNIT_HAPPINESS event fires within
+        -- a feeding-tick we can assume that the offset 
+        -- between happinessPoints and minimum-happiness
+        -- cant be greater than 35.
+        if PHTCDB.Pet.HappinessPoints > (happinessPoints + 35) then
+            PHTCDB.Pet.HappinessPoints = happinessPoints
+        end
+    end
+
+    -- Logic for when HappinessLevel is decreased
+    if happiness < PHTCDB.Pet.HappinessLevel then
+        PHTCDB.Pet.HappinessLevel = happiness
+
+        -- Also assuming that UNIT_HAPPINESS should fire
+        -- after the CLEU event that would allow us to
+        -- detect things such as Death and/or Dismissal.
+        -- We can assume that current happinessPoints should
+        -- NOT be higher than minumum + 349.
+        if PHTCDB.Pet.HappinessPoints > (happinessPoints + 349) then
+            PHTCDB.Pet.HappinessPoints = happinessPoints + 349
+        end
+    end
+end
+
 function PHT:StartIdleTimer()
     IdleTimer = C_Timer.NewTicker(
         7.2,
         function()
-            if not UnitExists("pet") then return end 
+            if not UnitExists("pet") then return end
+            -- Start benchmark:
+            if PHTCDB.Pet.HappinessPoints == 1050 then
+                print("Setting benchmark to: ".. time())
+                PHTCDB.Benchmark = {}
+                PHTCDB.Benchmark.LatestMax = time()
+            end
+
             DeductPoints(1)
         end
     )
