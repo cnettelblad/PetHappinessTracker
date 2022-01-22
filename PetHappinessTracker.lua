7106d33d55cecedb3aaa1280eabc9a809d7adf47
unknown <carl.nettelblad@gmail.com>
2022-01-22 14:12:50 +0100

    Now properly isolates functionality meant for hunters to hunters only

--- PetHappinessTracker.lua
+++ PetHappinessTracker.lua
@@ -3,23 +3,25 @@
 --]]
 local PHT = LibStub("AceAddon-3.0"):NewAddon("PetHappinessTracker")
 local SharedMedia = LibStub("LibSharedMedia-3.0")
-local frames = {}
 local localizedClass, englishClass, classIndex = UnitClass("player")
 
 --[[
     Addon init
 --]]
 function PHT:OnInitialize()
-    if classIndex ~= 3 then
-       return -- No need to init further for non-hunters.
-    end
     PHTDB = PHTDB_DEFAULTS
+
+    if classIndex ~= 3 then return end
+    -- Hunter specific initialization
     PHTCDB = PHTCDB_DEFAULTS
 end
 
 function PHT:OnEnable()
     PHT:CreateOptionsGUI()
     PHT:CreateMinimapIcon()
+
+    if classIndex ~= 3 then return end
+    -- The following only runs if the player is hunter
     PHT:CreateStatusFrame()
     PHT:StartIdleTimer()
 end
